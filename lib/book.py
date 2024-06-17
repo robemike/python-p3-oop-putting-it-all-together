892b033a65de40f6e03305b32803442d193b18ae
robemike <robemike1135@gmail.com>
2024-06-17 20:37:43 +0300

    completes lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
     
+    @property
+    def page_count(self):
+        """"The page count property."""
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, page_count):
+        """Page count must be an integer."""
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
         
+    def turn_page(self):
+        """Method to simulate flipping a page"""
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    @property
+    def size(self):
+        """The size property"""
+        return self._size
+    
+    @size.setter
+    def size(self, size):
+        """Size must be an integer"""
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+
+    def cobble(self):
+        """Marks the shoe as repaired"""
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+    def repair(self):
+        """Another method to mark the shoe as good as new"""
+        self.cobble()
